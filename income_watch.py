188354ab72321b1d323da2e23ad55b5c504a3463
Spencer Hawkins <hawkins.spencer@gmail.com>
2015-10-20 15:02:06 -0500

    Add estimations for current trade prices

--- income_watch.py
+++ income_watch.py
@@ -1,11 +1,13 @@
 import logging
 import os
 from pw_client import PWClient
+from lxml import etree
 
 __author__ = 'shawkins'
 
 money_string = '<b style="color: #28d020;">$</b>'
 img_dict = {'steel': 'https://politicsandwar.com/img/resources/steel.png', 'oil': 'https://politicsandwar.com/img/resources/oil.png', 'aluminum': 'https://politicsandwar.com/img/resources/aluminum.png', 'lead': 'https://politicsandwar.com/img/resources/lead.png', 'bauxite': 'https://politicsandwar.com/img/resources/bauxite.png', 'food': 'https://politicsandwar.com/img/icons/16/steak_meat.png', 'money': 'https://politicsandwar.com/img/resources/money.png', 'munition': 'https://politicsandwar.com/img/resources/munitions.png', 'uranium': 'https://politicsandwar.com/img/resources/uranium.png', 'coal': 'https://politicsandwar.com/img/resources/coal.png', 'iron': 'https://politicsandwar.com/img/resources/iron.png', 'gasoline': 'https://politicsandwar.com/img/resources/gasoline.png'}
+realstring_dict = {'steel': 'steel', 'oil': 'oil', 'lead': 'lead', 'aluminum': 'aluminum', 'munition': 'munitions', 'food': 'food', 'bauxite': 'bauxite', 'uranium': 'uranium', 'coal': 'coal', 'iron': 'iron', 'gasoline': 'gasoline'}
 
 logger = logging.getLogger("pwc")
 fhandler1 = logging.FileHandler("city_check.out", mode='w')
@@ -48,5 +50,21 @@ for key in totals.keys():
         html_string += "<tr>\n<td>\n<img src='"+img_dict[key]+"'>\n</td>\n<td>\n"+"{:,}".format(totals[key]*12)+"\n</td>\n</tr>\n"
 
 html_string += "</table>"
-print html_string
 
+total_money_value = totals['money']
+
+for item_type in realstring_dict.keys():
+    item_value = realstring_dict[item_type]
+    trade_url = "https://politicsandwar.com/index.php?id=90&display=world&resource1="+item_value+"&buysell=buy&ob=price&od=DESC&maximum=15&minimum=0&search=Go"
+    nationtable = pwc._retrieve_nationtable(trade_url, 0)
+    trade_tr = nationtable.findall(".//tr")[1]
+    trade_td = trade_tr.findall(".//td")[5]
+    trade_text = trade_td[0].text
+    trade_num = int(trade_text.split("/")[0].replace(",",""))
+    total_money_value += trade_num * totals[item_type]
+
+html_string += "\n<h1>Estimated total value collected this turn: "+"{:,}".format(total_money_value)+"</h1>\n"
+html_string += "\n<h1>Estimated total value to collect today: "+"{:,}".format(total_money_value * 12)+"</h1>\n"
+html_string += "<h4>Note: this value is calculated by taking the current 'buy' trading prices and multiplying them by the amount of resources collected this turn.</h4>"
+
+print html_string
