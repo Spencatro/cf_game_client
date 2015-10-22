308b5278026c66ac9bf3a1bc17f5afa7463d9215
Spencer Hawkins <hawkins.spencer@gmail.com>
2015-10-22 18:48:01 -0500

    Add graphs to market watch

--- market_watch.py
+++ market_watch.py
@@ -1,7 +1,10 @@
 import logging
 import os
-import pprint
+from colour import Color
 from pw_client import PWClient, LeanPWDB
+import plotly.plotly as py
+import plotly.graph_objs as go
+import plotly.tools as tls
 
 money_string = '<b style="color: #28d020;">$</b>'
 img_dict = {'steel': 'https://politicsandwar.com/img/resources/steel.png', 'oil': 'https://politicsandwar.com/img/resources/oil.png', 'aluminum': 'https://politicsandwar.com/img/resources/aluminum.png', 'lead': 'https://politicsandwar.com/img/resources/lead.png', 'bauxite': 'https://politicsandwar.com/img/resources/bauxite.png', 'food': 'https://politicsandwar.com/img/icons/16/steak_meat.png', 'money': 'https://politicsandwar.com/img/resources/money.png', 'munition': 'https://politicsandwar.com/img/resources/munitions.png', 'uranium': 'https://politicsandwar.com/img/resources/uranium.png', 'coal': 'https://politicsandwar.com/img/resources/coal.png', 'iron': 'https://politicsandwar.com/img/resources/iron.png', 'gasoline': 'https://politicsandwar.com/img/resources/gasoline.png'}
@@ -17,6 +20,15 @@ USERNAME = os.environ['PW_USER']
 PASS = os.environ['PW_PASS']
 pwc = PWClient(USERNAME, PASS, logger=logger)
 pwdb = LeanPWDB()
+
+sky_blue = Color("#2b8dcc")
+dark_blue = Color("#0d2b3e")
+
+light_orange = Color("#FF7B0C")
+dark_orange = Color("#7F4108")
+
+orange_gradient = list(dark_orange.range_to(light_orange, 100))
+blue_gradient = list(dark_blue.range_to(sky_blue, 100))
 
 resource_dict = {}
 for item_type in realstring_dict.keys():
@@ -45,4 +57,98 @@ for item_type in realstring_dict.keys():
     trade_num = int(trade_text.split("/")[0].replace(",",""))
     resource_dict[item_type]["sell"] = trade_num
 
-pwdb.add_market_watch_record(resource_dict)
+# pwdb.add_market_watch_record(resource_dict)
+
+html_string = "<table>\n"
+html_string += \
+    "<tr>" \
+    "<th>Resource</th>" \
+    "<th>Current (low) sell price</th>" \
+    "<th>Current (low) sell price</th>" \
+    "<th>Percent difference</th>" \
+    "</tr>\n"
+
+buys_higher_than_avg_sells = {}
+
+averages = {}
+for item_type in realstring_dict.keys():
+    res = pwdb.market_watch_collection.aggregate([{"$group": {"_id": None, "sell": {"$avg": "$values."+item_type+".sell"}, "buy": {"$avg": "$values."+item_type+".buy"}}}])
+    values = res.next()
+    averages[item_type] = {"sell": values["sell"], "buy": values["buy"]}
+    # Make judgements on sells
+
+    current_sell = resource_dict[item_type]["sell"]
+    average_sell = averages[item_type]["sell"]
+    sell_diffp = (abs(averages[item_type]["sell"] - resource_dict[item_type]["sell"]) / (.5 * (averages[item_type]["sell"] + resource_dict[item_type]["sell"]))) * 100
+    gradient_index = 0
+
+    if sell_diffp > 25:
+        gradient_index = 99
+    else:
+        gradient_index = int(sell_diffp / 25.0) - 1
+
+    if average_sell > current_sell:
+        sell_color = str(blue_gradient[gradient_index])
+        sell_diffp *= -1
+    else:
+        sell_color = str(blue_gradient[gradient_index])
+
+    current_buy = resource_dict[item_type]["buy"]
+    if current_buy > average_sell:
+        buys_higher_than_avg_sells[item_type] = current_buy
+
+    buy_diffp = (abs(averages[item_type]["buy"] - resource_dict[item_type]["buy"]) / (.5 * (averages[item_type]["buy"] + resource_dict[item_type]["buy"]))) * 100
+    html_string += "<tr>" \
+                   "<td>"+realstring_dict[item_type].capitalize()+"</td>" \
+                   "<td style='color="+sell_color+";'>"+str(current_sell)+"</td>" \
+                   "<td style='color="+sell_color+";'>"+str(average_sell)+"</td>" \
+                   "<td style='color="+sell_color+";'>"+str(sell_diffp)+"</td>" \
+                   "</tr>"
+html_string += "</table>"
+
+if len(buys_higher_than_avg_sells.keys()) > 0:
+    for key in buys_higher_than_avg_sells.keys():
+        html_string += "<h3>There is a buy offer for", key, "("+str(buys_higher_than_avg_sells[key])+") that exceeds the average sell price! Take this trade!</h3>"
+
+previous_records = []
+skipped_one = False
+for rec in pwdb.market_watch_collection.find().limit(200):
+    if not skipped_one:
+        skipped_one = True
+        continue
+    turn_record = {}
+    for item_type in realstring_dict.keys():
+        average_upto_record = pwdb.market_watch_collection.aggregate([{"$match": {"_id": {"$lt": rec["_id"]}}}, {"$group": {"_id": None, "sell": {"$avg": "$values."+item_type+".sell"}, "buy": {"$avg": "$values."+item_type+".buy"}}}]).next()
+        turn_record[item_type] = {"avg": average_upto_record, "turn": rec["values"][item_type]}
+    previous_records.append(turn_record)
+
+print html_string
+
+for item_type in realstring_dict.keys():
+    t1x = range(len(previous_records))
+    t1y = [record[item_type]["avg"]["sell"] for record in previous_records]
+    trace1 = go.Scatter(x=t1x,
+                        y=t1y,
+                        mode='lines+markers',
+                        name="Average price at turn",
+                        line=dict(
+                            shape='spline'
+                        ))
+    trace2 = go.Scatter(x=t1x,
+                        y=[record[item_type]["turn"]["sell"] for record in previous_records],
+                        mode='lines+markers',
+                        name="Current price at turn",
+                        line=dict(
+                            shape='spline'
+                        ))
+    data = [trace1, trace2]
+    layout = go.Layout(
+        title=(realstring_dict[item_type] + ': price and average over time').capitalize(),
+        showlegend=True
+    )
+    fig = go.Figure(data=data, layout=layout)
+    plot_url = py.plot(fig, filename=item_type+"_build_"+str(os.environ.get("BUILD_NUMBER")), auto_open=False)
+    plot_embed = tls.get_embed(plot_url)
+    print plot_embed
+
+
