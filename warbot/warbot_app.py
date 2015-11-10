d86df5940123050ad79ff06ba8137000223570b2
Spencer Hawkins <hawkins.spencer@gmail.com>
2015-11-09 18:04:27 -0600

    test a trigger method

--- warbot/warbot_app.py
+++ warbot/warbot_app.py
@@ -1,14 +1,31 @@
 from flask import Flask, jsonify, request
-import sys
+import os
 
 __author__ = 'shawkins'
 
 app = Flask(__name__)
 app.debug = True  # TODO: unset this after release!
 
+def verify_token(token):
+    if token != os.environ.get("incoming_slack_token"):
+        #do something
+        pass
+
+
+def remove_trigger(trigger, text):
+    text = text.lower()
+    trigger = trigger.lower()
+    return text[text.index(trigger) + len(trigger) + 1:]
+
 
 @app.route('/', methods=['POST'])
 def hello_world():
-    print request.form
-    sys.stderr.write(request.form)
+    token = request.form.get("token")
+    verify_token(token)
+    originating_user_id = request.form.get("user_id")
+    message_body = request.form.get("text")
+    trigger = request.form.get("trigger_word")
+
+    print remove_trigger(trigger, message_body)
+
     return jsonify({"text": "hello, human"})
