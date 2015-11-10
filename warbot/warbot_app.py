e848fc3ec2ec97cd6bd9593065f5ec936929ccd6
Spencer Hawkins <hawkins.spencer@gmail.com>
2015-11-09 18:40:03 -0600

    smol fix

--- warbot/warbot_app.py
+++ warbot/warbot_app.py
@@ -35,33 +35,33 @@ def remove_trigger(trigger, text):
 
 
 def notify_end_of_beige(slack_uid, action):
-    pass
+    return jsonify({"text": "Sorry, my human teacher is still teaching me that one!"})
 
 
 def watch_my_war(slack_uid, action):
-    pass
+    return jsonify({"text": "I'm afraid I haven't been taught how to do that yet :("})
 
 
 def show_pipeline(action):
-    pass
+    return jsonify({"text": "Sorry, I'm still learning how to do that one!"})
 
 
 def get_help(action):
 
     helpstring = "Currently available commands:\n" \
                  "Help: shows this message! Examples:\n" \
-                 "      'Warbot, help me!'\n" \
-                 "      'Warbot, help'\n" \
+                 "      `Warbot, help me!`\n" \
+                 "      `Warbot, help`\n" \
                  "War notifier: notifies you when you will have enough points in a war for a ground battle. Examples:\n" \
-                 "      'Warbot, notify me about war [war id number]'\n" \
-                 "      'Warbot, war [war id number]'\n" \
+                 "      `Warbot, notify me about war [war id number]`\n" \
+                 "      `Warbot, war [war id number]`\n" \
                  "Beige watcher: notifies you when a certain nation is about to leave beige. Examples:\n" \
-                 "      'Warbot, tell me when beige ends for [nation id number]'\n" \
-                 "      'Warbot, tell me when I can fight [nation id number]'\n" \
-                 "      'Warbot, beige [nation id number]'\n" \
+                 "      `Warbot, tell me when beige ends for [nation id number]`\n" \
+                 "      `Warbot, tell me when I can fight [nation id number]`\n" \
+                 "      `Warbot, beige [nation id number]`\n" \
                  "Show pipeline: shows nations in the current pipeline. Examples:\n" \
-                 "      'Warbot, show me the pipeline'\n" \
-                 "      'Warbot, pipeline'\n"
+                 "      `Warbot, show me the pipeline`\n" \
+                 "      `Warbot, pipeline`\n"
 
     return jsonify({"text": helpstring})
 
