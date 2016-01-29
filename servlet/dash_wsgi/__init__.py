e5138ca17a492e658b8f47fdf65bbf8625126806
Spencer Hawkins <hawkins.spencer@gmail.com>
2016-01-29 00:50:22 -0800

    only get prices once

--- servlet/dash_wsgi/__init__.py
+++ servlet/dash_wsgi/__init__.py
@@ -114,7 +114,19 @@ def defendability():
 def rsc_pulse():
     USERNAME = os.environ['PW_USER']
     PASS = os.environ['PW_PASS']
+    
     pwc = PWClient(USERNAME, PASS)
+    trade_nums = {}
+    for item_type in realstring_dict.keys():
+        item_value = realstring_dict[item_type]
+        trade_url = "https://politicsandwar.com/index.php?id=90&display=world&resource1="+item_value+"&buysell=buy&ob=price&od=DESC&maximum=15&minimum=0&search=Go"
+        nationtable = pwc._retrieve_nationtable(trade_url, 0)
+        trade_tr = nationtable.findall(".//tr")[1]
+        trade_td = trade_tr.findall(".//td")[5]
+        trade_text = trade_td[0].text
+        trade_num = int(trade_text.split("/")[0].replace(",",""))
+        trade_nums[item_type] = trade_num
+
     pwdb = LeanPWDB()
     latest_list = pwdb.get_latest_nation_cache_list()
     nations_out = []
@@ -122,12 +134,7 @@ def rsc_pulse():
         complex = nation["simple_net_income"]
         for item_type in realstring_dict.keys():
             item_value = realstring_dict[item_type]
-            trade_url = "https://politicsandwar.com/index.php?id=90&display=world&resource1="+item_value+"&buysell=buy&ob=price&od=DESC&maximum=15&minimum=0&search=Go"
-            nationtable = pwc._retrieve_nationtable(trade_url, 0)
-            trade_tr = nationtable.findall(".//tr")[1]
-            trade_td = trade_tr.findall(".//td")[5]
-            trade_text = trade_td[0].text
-            trade_num = int(trade_text.split("/")[0].replace(",",""))
+            trade_num = trade_nums[item_type]
             change = trade_num * nation["net_resource_production"][item_value]
             complex += change
         nation["complex_net_income"] = complex
